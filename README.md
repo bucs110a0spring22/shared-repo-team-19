add853e809496d2d70ca6f6496809923a522ac23
danielm179 <91908325+danielm179@users.noreply.github.com>
2022-02-08 16:36:55 +0000

    main.py

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 
 ## Team Members
 
-* Team Member 1:
-* Team Member 2:
-* Team Member 3:
+* Team Member 1: Daniel Monaghan
+* Team Member 2: Ian Avery
+* Team Member 3: Archit Kothari
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+mylist = []
+print("Enter four numbers")
+for i in range(0,4):
+  print("Enter number at index", i, )
+  item = int(input())
+  mylist.append(item)
+print("List: ", mylist)
+
+for item in mylist:
+  print(item)
 
